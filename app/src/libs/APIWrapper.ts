e8e391a648c7ecefe96d9d708740370d460646f8
Pr0ny <13719880+Pr0ny@users.noreply.github.com>
2023-02-09 21:00:45 +0100

    chore: API JWT authentication

--- app/src/libs/APIWrapper.ts
+++ app/src/libs/APIWrapper.ts
@@ -1,19 +1,39 @@
-import axios, {AxiosPromise, AxiosRequestConfig, AxiosResponse} from "axios";
+import axios, {AxiosPromise, AxiosRequestConfig} from "axios";
 
 class APIWrapper {
 
-  #jwt
-  #baseUrl
+  #jwt: string
+  #baseUrl: string
+  #expireAt: number | null
 
   constructor() {
+    this.jwt = null;
+    this.#expireAt = null;
   }
 
-  get jwt(): string {
+  get jwt(): string | null {
     return this.#jwt;
   }
 
-  set jwt(jwt: string) {
-    this.#jwt = jwt;
+  set jwt(jwt: string | null) {
+    if (jwt !== null) {
+      const payload = this.getJwtPayload(jwt);
+      this.setJwtAuthenticationExpiration(payload['exp']);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
+      this.#jwt = jwt;
+    } else {
+      this.#jwt = null;
+    }
+  }
+
+  set expirationDate(expirationDate: number) {
+    this.#expireAt = expirationDate
+  }
+
+  isExpired() {
+    const now = Date.now() / 1000; // Milliseconds to seconds
+
+    return this.#expireAt === null || now > this.#expireAt;
   }
 
   set url(url: string) {
@@ -25,29 +45,41 @@ class APIWrapper {
   }
 
   async post(endpoint: string, data: object): Promise<AxiosPromise> {
-    console.log('Creation du POST')
-    const option: AxiosRequestConfig = this.craftOption('POST', endpoint, data);
-    console.log(JSON.stringify(option))
-    return await axios.request(option)
+    const option: AxiosRequestConfig = this.craftConfig('POST', endpoint, data);
+    return await axios.request(option);
   }
 
-  get(endpoint, qp, params): void {
-    const option = this.craftOption('GET', endpoint, params);
-    return
+  async get(endpoint: string, params: object | null = null): Promise<AxiosPromise> {
+    const option = this.craftConfig('GET', endpoint, null, params);
+    return await axios.request(option);
   }
 
-  craftUrl(endpoint): string {
-    return `${this.#baseUrl}${endpoint}`
+  async put(): Promise<void> {}
+
+  async delete(): Promise<void> {}
+
+  craftUrl(endpoint: string): string {
+    return `${this.#baseUrl}${endpoint}`;
   }
 
-  craftOption(method, endpoint, data): AxiosRequestConfig {
-    const option = {
+  craftConfig(method: string, endpoint: string | null, data: object | null = null, params: object | null = null): AxiosRequestConfig {
+    return {
       method: method,
       url: this.craftUrl(endpoint),
-      data: data
-    }
-    return option
+      data: data,
+      params: params
+    };
+  }
+
+  setJwtAuthenticationExpiration(expirationDate: number): void {
+    // We can do further date/timestamp verification here
+    this.expirationDate = expirationDate;
+  }
+
+  getJwtPayload(token: string) {
+    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
   }
 
 }
+
 export default APIWrapper
