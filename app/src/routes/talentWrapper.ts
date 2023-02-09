fd159455c712d9af86774c674b77e11387133ad0
Pr0ny <13719880+Pr0ny@users.noreply.github.com>
2023-02-09 21:04:19 +0100

    chore: routes

--- app/src/routes/talentWrapper.ts
+++ app/src/routes/talentWrapper.ts
@@ -1,23 +1,53 @@
 import {Server, ServerApplicationState} from "@hapi/hapi";
-import {skillVersionValidation} from "../validators/query";
-import {talentSkillHandler} from "../hanlders/talenthandler";
+import {talentsSkillVersionValidation, talentsUsersActiveValidation, talentsUsersValidation} from "../validators/query";
+import {talentActiveUsers, talentSkillCategory, talentSkillHandler, talentUsers} from "../hanlders/talentHandler";
 
-const pathPrefix = '/talent'
+const pathPrefix = '/talents'
 
 const talentRoutes = (server: Server<ServerApplicationState>): void => {
 
-  // Get a single info
+  // Get all skills from 365Talents
   server.route({
     method: 'GET',
     path: `${pathPrefix}/skills`,
     options: {
       validate: {
-        query: skillVersionValidation,
+        query: talentsSkillVersionValidation,
       }
     },
     handler: talentSkillHandler
   });
 
+  // Get all skill categories from 365Talents
+  server.route({
+    method: 'GET',
+    path: `${pathPrefix}/skills/categories`,
+    handler: talentSkillCategory
+  });
+
+  // Get users from 365Talents
+  server.route({
+    method: 'GET',
+    path: `${pathPrefix}/users`,
+    options: {
+      validate: {
+        query: talentsUsersValidation,
+      }
+    },
+    handler: talentUsers
+  });
+
+  // Get active users from 365Talents
+  server.route({
+    method: 'GET',
+    path: `${pathPrefix}/users/active`,
+    options: {
+      validate: {
+        query: talentsUsersActiveValidation,
+      }
+    },
+    handler: talentActiveUsers
+  });
 }
 
 export default talentRoutes;
