8f8b571ec85332417acbe71bccdfcef1fb3dc76b
Pr0ny <13719880+Pr0ny@users.noreply.github.com>
2023-02-09 21:04:46 +0100

    chore: API core

--- app/src/services/TalentWrapper.ts
+++ app/src/services/TalentWrapper.ts
@@ -1,5 +1,3 @@
-import {environmentListValidation} from "../validators/wrapper"
-import Joi from "joi";
 import APIWrapper from "../libs/APIWrapper"
 
 /**
@@ -8,19 +6,94 @@ import APIWrapper from "../libs/APIWrapper"
  */
 class TalentWrapper extends APIWrapper { // Extend an APIWrapper for finner env management
 
-  constructor(url) {
+  #partnerLogin
+  #partnerPassword
+  #partnerMode
+
+  constructor(url: string, partner: boolean) {
     super();
     this.url = url;
+    this.partner = partner;
   }
 
-  async partnerAuthenticate(partner: string, password: string) {
-    const endpoint = '/v1/auth/jwt'
-    console.log(partner)
-    const response = await this.post(endpoint, {partner: partner, password: password})
-    console.log(JSON.stringify(response))
+  toggleMode(): void {
+    this.#partnerMode = !this.#partnerMode;
   }
 
-  async userAuthenticate() {}
+  set partner(mode: boolean) {
+    this.#partnerMode = mode;
+  }
+
+  get partner(): boolean {
+    return this.#partnerMode;
+  }
+
+  setPartnerCredential(login: string, password: string) {
+    this.#partnerLogin = login;
+    this.#partnerPassword = password;
+  }
+
+  async partnerAuthenticate(partner: string, password: string): Promise<boolean> {
+    const endpoint = '/v1/auth/jwt';
+    const response = await this.post(endpoint, {partner: partner, password: password});
+
+    if (response.status === 200) {
+      this.jwt = response['data'];
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async userAuthenticate(): Promise<void> {}
+
+  async verifyAuthentication(): Promise<void> {
+    if (this.jwt === null || this.isExpired()) {
+      if (this.partner) {
+        await this.partnerAuthenticate(this.#partnerLogin, this.#partnerPassword)
+      } else {
+        await this.userAuthenticate();
+      }
+    }
+  }
+
+  async getSkills(version: number = 0): Promise<object | string | null> {
+    await this.verifyAuthentication();
+
+    const response = await this.get('/v1/skills', {version: version});
+    return response['data'];
+  }
+
+  async getSkillsCategories(): Promise<object | string | null> {
+    await this.verifyAuthentication();
+
+    const response = await this.get('/v1/skills/categories');
+    return response['data'];
+  }
+
+  async getUsers(page: number = 0,
+                 limit: number = 100,
+                 hasValidatedCharter: boolean | null = null,
+                 modifiedAfter: string | null = null): Promise<object | string | null> {
+    await this.verifyAuthentication();
+
+    const response = await this.get('/v1/users',
+      {
+        page: page,
+        limit: limit,
+        hasValidatedCharter: hasValidatedCharter,
+        modifiedAfter: modifiedAfter
+      });
+    return response['data'];
+  }
+
+  async getActiveUsers(page: number = 0, limit: number = 100): Promise<object | string | null> {
+    await this.verifyAuthentication();
+
+    const response = await this.get('/v1/users/active', {page: page, limit: limit});
+    return response['data'];
+  }
+
 }
 
 export {TalentWrapper}
