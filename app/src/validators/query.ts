6094f31bb5c6660b8b21bd497eb0ee3498e0893d
Pr0ny <13719880+Pr0ny@users.noreply.github.com>
2023-02-09 21:05:54 +0100

    chore: validators

--- app/src/validators/query.ts
+++ app/src/validators/query.ts
@@ -1,7 +1,22 @@
 import Joi from "joi";
 
-const skillVersionValidation = Joi.object({
+const talentsSkillVersionValidation = Joi.object({
   version: Joi.number().equal(0),
-});
+}).options({ stripUnknown: true });
 
-export {skillVersionValidation}
+// const pageValidation = Joi.number().min(0)
+// const limitValidation = Joi.number().min(1).max(100)
+
+const talentsUsersValidation = Joi.object({
+  page: Joi.number().min(0),
+  pageSize: Joi.number().min(1).max(100),
+  hasValidatedCharter: Joi.boolean(),
+  modifiedAfter: Joi.date()
+}).options({ stripUnknown: true });
+
+const talentsUsersActiveValidation = Joi.object({
+  page: Joi.number().min(0),
+  pageSize: Joi.number().min(1).max(100)
+}).options({ stripUnknown: true });
+
+export {talentsSkillVersionValidation, talentsUsersValidation, talentsUsersActiveValidation}
